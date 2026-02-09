8cc3e5635761ed03c3e33a8dbfa5f0608a4a846d
YarikTunev <Yariktunev2005@mail,ru>
2026-02-10 00:00:09 +0500

    востановление пароля и фиксы

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -8,42 +8,37 @@ $user_ip = $_SERVER['REMOTE_ADDR'];
 $ip_check = $mysqli->query("SELECT attempts, last_attempt FROM `ip_block` WHERE `ip`='$user_ip'");
 $ip_data = $ip_check->fetch_assoc();
 
-if ($ip_data && $ip_data['attempts'] >= 10) {
-    $timeout = 30 * 60; 
+if ($ip_data && $ip_data['attempts'] >= 5) {
+    $timeout = 15 * 60; 
     if (time() - strtotime($ip_data['last_attempt']) < $timeout) {
         exit("blocked_ip");
-    } else {
-        // Время вышло, сбрасываем счетчик
-        $mysqli->query("UPDATE `ip_block` SET `attempts` = 0 WHERE `ip` = '$user_ip'");
     }
 }
 
+if(!isset($_POST['login']) || !isset($_POST['password'])) exit("wrong");
+
 $login = $mysqli->real_escape_string($_POST['login']);
 $password = $_POST['password']; 
-
 
 $user_check = $mysqli->query("SELECT id FROM `users` WHERE `login`='$login'");
 $user_data = $user_check->fetch_assoc();
 
 if ($user_data) {
-    $query_user = $mysqli->query("SELECT id FROM `users` WHERE `login`='$login' AND `password`='$password'");
-    $auth_data = $query_user->fetch_row();
+    $query_auth = $mysqli->query("SELECT id FROM `users` WHERE `login`='$login' AND `password`='$password'");
+    $auth_data = $query_auth->fetch_row();
 
     if ($auth_data) {
         $id = $auth_data[0];
         $mysqli->query("DELETE FROM `ip_block` WHERE `ip` = '$user_ip'");
+        
         $_SESSION['user'] = $id;
         echo md5(md5($id));
     } else {
-        $mysqli->query("INSERT INTO `ip_block` (ip, attempts, last_attempt) 
-                        VALUES ('$user_ip', 1, NOW()) 
+        $mysqli->query("INSERT INTO `ip_block` (ip, attempts, last_attempt) VALUES ('$user_ip', 1, NOW()) 
                         ON DUPLICATE KEY UPDATE attempts = attempts + 1, last_attempt = NOW()");
-        echo "wrong"; 
+        echo "wrong";
     }
 } else {
-    $mysqli->query("INSERT INTO `ip_block` (ip, attempts, last_attempt) 
-                    VALUES ('$user_ip', 1, NOW()) 
-                    ON DUPLICATE KEY UPDATE attempts = attempts + 1, last_attempt = NOW()");
     echo "wrong";
 }
 ?>
--- ajax/recovery.php
+++ ajax/recovery.php
@@ -1,55 +1,46 @@
 <?php
-    session_start();
-    include("../settings/connect_datebase.php");
+session_start();
+include("../settings/connect_datebase.php");
 
-    $action = $_POST['action'] ?? '';
-    $login = $mysqli->real_escape_string($_POST['login'] ?? '');
+$action = $_POST['action'] ?? '';
+$login = $mysqli->real_escape_string($_POST['login'] ?? '');
+$user_ip = $_SERVER['REMOTE_ADDR'];
+
+if ($action == "get_question") {
+    $query = $mysqli->query("SELECT `secret_question` FROM `users` WHERE `login`='$login'");
+    if ($query && $user = $query->fetch_assoc()) {
+        echo $user['secret_question'];
+    } else {
+        echo "error_not_found";
+    }
+    exit;
+}
+
+if ($action == "verify") {
+    $answer = $mysqli->real_escape_string(mb_strtolower(trim($_POST['answer'] ?? '')));
+    $mode = $_POST['mode'] ?? 'password'; 
+
+    $query_user = $mysqli->query("SELECT id FROM `users` WHERE `login`='$login' AND `secret_answer`='$answer'");
     
-    function PasswordGeneration() {
-        $chars = "qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP";
-        $max = 10;
-        $size = strlen($chars) - 1;
-        $password = "";
-        while($max--) {
-            $password .= $chars[rand(0, $size)];
-        }
-        return $password;
-    }
+    if ($user_data = $query_user->fetch_assoc()) {
+        $user_id = $user_data['id'];
 
-    if ($action == "get_question") {
-        $query = $mysqli->query("SELECT `secret_question` FROM `users` WHERE `login`='$login'");
-        if ($query && $user = $query->fetch_assoc()) {
-            echo $user['secret_question'];
+        $mysqli->query("DELETE FROM `ip_block` WHERE `ip` = '$user_ip'");
+        $mysqli->query("UPDATE `users` SET `attempts` = 0 WHERE `id` = $user_id");
+
+        if ($mode == 'unlock') {
+            echo "success_unlock";
         } else {
-            echo "error_not_found";
+            $new_pass = substr(md5(rand()), 0, 8); 
+            $hashed = md5($new_pass); 
+            $mysqli->query("UPDATE `users` SET `password`='$hashed' WHERE `id` = $user_id");
+            echo "success_password";
         }
-        exit;
+    } else {
+        $mysqli->query("INSERT INTO `ip_block` (ip, attempts, last_attempt) VALUES ('$user_ip', 1, NOW()) 
+                        ON DUPLICATE KEY UPDATE attempts = attempts + 1, last_attempt = NOW()");
+        echo "wrong_answer";
     }
-
-    if ($action == "verify") {
-        $answer = $mysqli->real_escape_string(mb_strtolower(trim($_POST['answer'] ?? '')));
-        $mode = $_POST['mode'] ?? 'password'; 
-
-        $query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='$login' AND `secret_answer`='$answer'");
-        
-        if($user_read = $query_user->fetch_row()) {
-            $id = $user_read[0];
-
-            if ($mode == 'unlock') {
-                $mysqli->query("UPDATE `users` SET `attempts` = 0 WHERE `id` = $id");
-                echo "success_unlock";
-            } else {
-                $password = PasswordGeneration();
-                $hashed_password = md5($password);
-                
-                $mysqli->query("UPDATE `users` SET `password`='$hashed_password', `attempts` = 0 WHERE `id` = $id");
-                echo "success_password"; 
-            }
-        } else {
-            echo "wrong_answer";
-        }
-        exit;
-    }
-
-    echo "no_action";
+    exit;
+}
 ?>
